0c6daa37814bc1ff8479a998dfa347deb64a89e6
Khopa <clemguip@gmail.com>
2015-12-15 11:15:25 +0100

    Added disclaimer on tests

--- src/test/java/com/khopa/spring/event/poc/TestThreadPooledEventManager.java
+++ src/test/java/com/khopa/spring/event/poc/TestThreadPooledEventManager.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+/**
+ * These isn't unit nor integration tests, but merely a "test" use case as proof of concept
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:pocContextThreadPooled.xml")
 public class TestThreadPooledEventManager {
